4a58530e197f02af10e198cc134fc4df16eaffa9
Thalles Stanziola <86901163+Thallestan@users.noreply.github.com>
2024-11-21 21:42:29 -0300

    Melhorias - HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -8,24 +8,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-//Estrutura da tabela
+// Estrutura para armazenar os dados da pessoa
+typedef struct {
+    char nome[MAX_INPUT_SIZE];
+    char data_nascimento[11];  // Formato: "DD/MM/AAAA"
+    char cpf[12];              // CPF com 11 dígitos + '\0'
+} DadosPessoa;
+
+// Estrutura da tabela hash
 typedef struct Node {
-    char *key;
-    char *value;
+    char cpf[12];
+    DadosPessoa value;
     struct Node *next;
 } Node;
 
-//Protótipos das funcoes
-void insert(const char *key, const char *value);
-char* search(const char *key);
-void delete(const char *key);
+// Protótipos das funções
+int cpf_ja_cadastrado(const char *cpf);
+int validar_data(const char *data);
+int validar_cpf(const char *cpf);
+int calcular_dv1(const char *cpf);
+int calcular_dv2(const char *cpf, int dv1);
+void insert(const char *cpf, const char *nome, const char *data_nascimento);
+DadosPessoa* search(const char *cpf);
+void delete(const char *cpf);
 void initialize_table();
 void resize_table();
+void display_table();
+void save_to_file(const char *filename);
+void load_from_file(const char *filename);
+int validar_nome(const char *nome);
 
-// Declaracao de variaveis
+// Declaração de variáveis globais
+char confirmacao;
 Node **hash_table;  // Tabela hash
 int TABLE_SIZE = INITIAL_TABLE_SIZE;  // Tamanho inicial da tabela
 int element_count = 0;  // Número de elementos na tabela
 
 #endif
+
